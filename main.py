a3ab5bdcced420291346cef8afb3bad66e288bca
Rahul Madan <madan.rahul9@gmail.com>
2022-02-14 10:41:21 +0530

    added post methos to fetch linked devices on 1 aadhaar

--- main.py
+++ main.py
@@ -20,29 +20,36 @@ app = FastAPI()
 
 # for CORS
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"],allow_headers=["*"])
-
-
 # for CORS
 
-class UserLogin(BaseModel):
-    aadhaarNumber: str
+class login_user(BaseModel):
+    aadhaar_number: str
     password: str
+
+class linked_devices(BaseModel):
+    user_aadhaar_number: str
 
 
 @app.post("/login")
-def root(requestBody: UserLogin):
+def root(request_body: login_user):
     mycursor.execute("SELECT * FROM user_login")
     columns = mycursor.description
     result = [{columns[index][0]: column for index, column in enumerate(value)} for value in mycursor.fetchall()]
     for credentials in result:
-        if requestBody.aadhaarNumber == credentials['aadhaarNumber']:
-            if requestBody.password == credentials['password']:
+        if request_body.aadhaar_number == credentials['aadhaar_number']:
+            if request_body.password == credentials['password']:
                 return {'statusCode': 0, 'message': 'Login Successful'}
             else:
                 return {'statusCode': 1, 'message': 'Password does not match'}
     else:
         return {'statusCode': 2, 'message': 'Aadhaar not registered'}
 
+@app.post("/get-linked-devices")
+def root(request_body: linked_devices):
+    mycursor.execute("SELECT * FROM phone_ownership WHERE owner_aadhaar = {}".format(request_body.user_aadhaar_number))
+    columns = mycursor.description
+    result = [{columns[index][0]: column for index, column in enumerate(value)} for value in mycursor.fetchall()]
+    return result
 
 
 @app.get("/hello/{name}")
